952d0ed3e86e176bd6ae98e84b3a9bcb2223059a
Benjamin Trent <4357155+benwtrent@users.noreply.github.com>
2021-12-08 12:24:15 -0500

    make format better

--- src/day8.rs
+++ src/day8.rs
@@ -39,32 +39,32 @@ fn day8_2(input: &Vec<(Vec<BTreeSet<char>>, Vec<BTreeSet<char>>)>) -> usize {
     input
         .iter()
         .map(|i| {
-            let m: BTreeSet<BTreeSet<char>> = i.0.iter().cloned().collect();
             let all: BTreeSet<BTreeSet<char>> =
-                m.union(&i.1.iter().cloned().collect()).cloned().collect();
+                i.0.iter()
+                    .cloned()
+                    .collect::<BTreeSet<BTreeSet<char>>>()
+                    .union(&i.1.iter().cloned().collect())
+                    .cloned()
+                    .collect();
             let mut known_patterns: HashMap<BTreeSet<char>, usize> = HashMap::new();
             let mut known_patterns_rev_lookup: HashMap<usize, BTreeSet<char>> = HashMap::new();
             let eight: Option<&BTreeSet<_>> = all.iter().find(|p| p.len() == 7);
             if let Some(eight) = eight {
-                println!("found 8 {:?}", eight);
                 known_patterns.insert(eight.clone(), 8);
                 known_patterns_rev_lookup.insert(8, eight.clone());
             }
             let seven: Option<&BTreeSet<_>> = all.iter().find(|p| p.len() == 3);
             if let Some(seven) = seven {
-                println!("found 7 {:?}", seven);
                 known_patterns.insert(seven.clone(), 7);
                 known_patterns_rev_lookup.insert(7, seven.clone());
             }
             let one: Option<&BTreeSet<_>> = all.iter().find(|p| p.len() == 2);
             if let Some(one) = one {
-                println!("found 1 {:?}", one);
                 known_patterns.insert(one.clone(), 1);
                 known_patterns_rev_lookup.insert(1, one.clone());
             }
             let four: Option<&BTreeSet<_>> = all.iter().find(|p| p.len() == 4);
             if let Some(four) = four {
-                println!("found 4 {:?}", four);
                 known_patterns.insert(four.clone(), 4);
                 known_patterns_rev_lookup.insert(4, four.clone());
             }
@@ -72,203 +72,141 @@ fn day8_2(input: &Vec<(Vec<BTreeSet<char>>, Vec<BTreeSet<char>>)>) -> usize {
             while known_patterns.len() < 10 {
                 iters += 1;
                 if iters > 1000 {
-                    println!(
-                        "still missing {:?}",
-                        all.difference(
-                            &known_patterns
-                                .keys()
-                                .cloned()
-                                .collect::<BTreeSet<BTreeSet<char>>>()
-                        )
-                    );
                     panic!("could not find all numbers")
                 }
                 if !known_patterns_rev_lookup.contains_key(&9) {
-                    let nine: Option<_> = if known_patterns_rev_lookup.contains_key(&4) {
+                    let nine: Option<_> = if let Some(four) = known_patterns_rev_lookup.get(&4) {
+                        all.iter().find(|p| p.len() == 6 && p.is_superset(four))
+                    } else if let (Some(zero), Some(six)) = (
+                        known_patterns_rev_lookup.get(&0),
+                        known_patterns_rev_lookup.get(&6),
+                    ) {
                         all.iter().find(|p| {
                             p.len() == 6
-                                && p.is_superset(known_patterns_rev_lookup.get(&4).unwrap())
-                        })
-                    } else if known_patterns_rev_lookup.contains_key(&0)
-                        && known_patterns_rev_lookup.contains_key(&6)
-                    {
-                        all.iter().find(|p| {
-                            p.len() == 6
-                                && p.difference(known_patterns_rev_lookup.get(&0).unwrap())
-                                    .count()
-                                    > 0
-                                && p.difference(known_patterns_rev_lookup.get(&6).unwrap())
-                                    .count()
-                                    > 0
+                                && p.difference(zero).count() > 0
+                                && p.difference(six).count() > 0
                         })
                     } else {
                         Option::None
                     };
                     if let Some(nine) = nine {
-                        println!("found 9 {:?}", nine);
                         known_patterns.insert(nine.clone(), 9);
                         known_patterns_rev_lookup.insert(9, nine.clone());
                     }
                 }
                 if !known_patterns_rev_lookup.contains_key(&0) {
-                    let zero: Option<_> = if known_patterns_rev_lookup.contains_key(&8)
-                        && known_patterns_rev_lookup.contains_key(&9)
-                        && known_patterns_rev_lookup.contains_key(&7)
-                    {
-                        let desired_subset: BTreeSet<char> = known_patterns_rev_lookup
-                            .get(&8)
-                            .unwrap()
-                            .difference(known_patterns_rev_lookup.get(&9).unwrap())
+                    let zero: Option<_> = if let (Some(eight), Some(nine), Some(seven)) = (
+                        known_patterns_rev_lookup.get(&8),
+                        known_patterns_rev_lookup.get(&9),
+                        known_patterns_rev_lookup.get(&7),
+                    ) {
+                        let desired_subset: BTreeSet<char> = eight
+                            .difference(nine)
                             .cloned()
                             .collect::<BTreeSet<char>>()
-                            .union(&known_patterns_rev_lookup.get(&7).unwrap().clone())
+                            .union(seven)
                             .cloned()
                             .collect();
                         all.iter()
                             .find(|p| p.len() == 6 && p.is_superset(&desired_subset))
-                    } else if known_patterns_rev_lookup.contains_key(&6)
-                        && known_patterns_rev_lookup.contains_key(&9)
-                    {
+                    } else if let (Some(six), Some(nine)) = (
+                        known_patterns_rev_lookup.get(&6),
+                        known_patterns_rev_lookup.get(&9),
+                    ) {
                         all.iter().find(|p| {
                             p.len() == 6
-                                && p.difference(known_patterns_rev_lookup.get(&6).unwrap())
-                                    .count()
-                                    > 0
-                                && p.difference(known_patterns_rev_lookup.get(&9).unwrap())
-                                    .count()
-                                    > 0
+                                && p.difference(six).count() > 0
+                                && p.difference(nine).count() > 0
                         })
                     } else {
                         Option::None
                     };
                     if let Some(zero) = zero {
-                        println!("found 0 {:?}", zero);
                         known_patterns.insert(zero.clone(), 0);
                         known_patterns_rev_lookup.insert(0, zero.clone());
                     }
                 }
                 if !known_patterns_rev_lookup.contains_key(&6) {
-                    let six: Option<_> = if known_patterns_rev_lookup.contains_key(&0)
-                        && known_patterns_rev_lookup.contains_key(&9)
-                    {
+                    let six: Option<_> = if let (Some(zero), Some(nine)) = (
+                        known_patterns_rev_lookup.get(&0),
+                        known_patterns_rev_lookup.get(&9),
+                    ) {
                         all.iter().find(|p| {
                             p.len() == 6
-                                && p.difference(known_patterns_rev_lookup.get(&0).unwrap())
-                                    .count()
-                                    > 0
-                                && p.difference(known_patterns_rev_lookup.get(&9).unwrap())
-                                    .count()
-                                    > 0
+                                && p.difference(zero).count() > 0
+                                && p.difference(nine).count() > 0
                         })
-                    } else if known_patterns_rev_lookup.contains_key(&8)
-                        && known_patterns_rev_lookup.contains_key(&2)
-                        && known_patterns_rev_lookup.contains_key(&1)
-                    {
-                        let special_diff: BTreeSet<char> = known_patterns_rev_lookup
-                            .get(&1)
-                            .unwrap()
-                            .intersection(&known_patterns_rev_lookup.get(&2).unwrap())
-                            .cloned()
-                            .collect();
-                        let finding: BTreeSet<char> = known_patterns_rev_lookup
-                            .get(&8)
-                            .unwrap()
-                            .difference(&special_diff)
-                            .cloned()
-                            .collect();
+                    } else if let (Some(eight), Some(two), Some(one)) = (
+                        known_patterns_rev_lookup.get(&8),
+                        known_patterns_rev_lookup.get(&2),
+                        known_patterns_rev_lookup.get(&1),
+                    ) {
+                        let special_diff: BTreeSet<char> = one.intersection(two).cloned().collect();
+                        let finding: BTreeSet<char> =
+                            eight.difference(&special_diff).cloned().collect();
                         all.iter().find(|p| p.len() == 6 && p.is_superset(&finding))
                     } else {
                         Option::None
                     };
                     if let Some(six) = six {
-                        println!("found 6 {:?}", six);
                         known_patterns.insert(six.clone(), 6);
                         known_patterns_rev_lookup.insert(6, six.clone());
                     }
                 }
                 if !known_patterns_rev_lookup.contains_key(&2) {
-                    let two = if known_patterns_rev_lookup.contains_key(&8)
-                        && known_patterns_rev_lookup.contains_key(&0)
-                        && known_patterns_rev_lookup.contains_key(&6)
-                        && known_patterns_rev_lookup.contains_key(&9)
-                    {
-                        let mut finding: BTreeSet<char> = known_patterns_rev_lookup
-                            .get(&8)
-                            .unwrap()
-                            .difference(known_patterns_rev_lookup.get(&0).unwrap())
-                            .cloned()
-                            .collect::<BTreeSet<char>>();
-                        finding.extend(
-                            known_patterns_rev_lookup
-                                .get(&8)
-                                .unwrap()
-                                .difference(known_patterns_rev_lookup.get(&6).unwrap())
-                                .cloned()
-                                .collect::<BTreeSet<char>>(),
-                        );
-                        finding.extend(
-                            known_patterns_rev_lookup
-                                .get(&8)
-                                .unwrap()
-                                .difference(known_patterns_rev_lookup.get(&9).unwrap())
-                                .cloned()
-                                .collect::<BTreeSet<char>>(),
-                        );
+                    let two = if let (Some(eight), Some(zero), Some(six), Some(nine)) = (
+                        known_patterns_rev_lookup.get(&8),
+                        known_patterns_rev_lookup.get(&0),
+                        known_patterns_rev_lookup.get(&6),
+                        known_patterns_rev_lookup.get(&9),
+                    ) {
+                        let mut finding: BTreeSet<char> =
+                            eight.difference(zero).cloned().collect::<BTreeSet<char>>();
+                        finding.extend(eight.difference(six).cloned().collect::<BTreeSet<char>>());
+                        finding.extend(eight.difference(nine).cloned().collect::<BTreeSet<char>>());
                         all.iter().find(|p| p.len() == 5 && p.is_superset(&finding))
-                    } else if known_patterns_rev_lookup.contains_key(&3)
-                        && known_patterns_rev_lookup.contains_key(&5)
-                    {
-                        all.iter().find(|p| {
-                            p.len() == 5
-                                && !p.is_superset(known_patterns_rev_lookup.get(&5).unwrap())
-                                && !p.is_superset(known_patterns_rev_lookup.get(&3).unwrap())
-                        })
+                    } else if let (Some(three), Some(five)) = (
+                        known_patterns_rev_lookup.get(&3),
+                        known_patterns_rev_lookup.get(&5),
+                    ) {
+                        all.iter()
+                            .find(|p| p.len() == 5 && !p.is_superset(five) && !p.is_superset(three))
                     } else {
                         Option::None
                     };
                     if let Some(two) = two {
-                        println!("found 2 {:?}", two);
                         known_patterns.insert(two.clone(), 2);
                         known_patterns_rev_lookup.insert(2, two.clone());
                     }
                 }
                 if !known_patterns_rev_lookup.contains_key(&3) {
-                    let three = if known_patterns_rev_lookup.contains_key(&2)
-                        && known_patterns_rev_lookup.contains_key(&5)
-                    {
-                        all.iter().find(|p| {
-                            p.len() == 5
-                                && !p.is_superset(known_patterns_rev_lookup.get(&5).unwrap())
-                                && !p.is_superset(known_patterns_rev_lookup.get(&2).unwrap())
-                        })
-                    } else if known_patterns_rev_lookup.contains_key(&1) {
-                        all.iter().find(|p| {
-                            p.len() == 5
-                                && p.is_superset(known_patterns_rev_lookup.get(&1).unwrap())
-                        })
+                    let three = if let (Some(two), Some(five)) = (
+                        known_patterns_rev_lookup.get(&2),
+                        known_patterns_rev_lookup.get(&5),
+                    ) {
+                        all.iter()
+                            .find(|p| p.len() == 5 && !p.is_superset(five) && !p.is_superset(two))
+                    } else if let Some(one) = known_patterns_rev_lookup.get(&1) {
+                        all.iter().find(|p| p.len() == 5 && p.is_superset(one))
                     } else {
                         Option::None
                     };
                     if let Some(three) = three {
-                        println!("found 3 {:?}", three);
                         known_patterns.insert(three.clone(), 3);
                         known_patterns_rev_lookup.insert(3, three.clone());
                     }
                 }
                 if !known_patterns_rev_lookup.contains_key(&5) {
-                    let five = if known_patterns_rev_lookup.contains_key(&3)
-                        && known_patterns_rev_lookup.contains_key(&2)
-                    {
-                        all.iter().find(|p| {
-                            p.len() == 5
-                                && !p.is_superset(known_patterns_rev_lookup.get(&2).unwrap())
-                                && !p.is_superset(known_patterns_rev_lookup.get(&3).unwrap())
-                        })
+                    let five = if let (Some(three), Some(two)) = (
+                        known_patterns_rev_lookup.get(&3),
+                        known_patterns_rev_lookup.get(&2),
+                    ) {
+                        all.iter()
+                            .find(|p| p.len() == 5 && !p.is_superset(two) && !p.is_superset(three))
                     } else {
                         Option::None
                     };
                     if let Some(five) = five {
-                        println!("found 5 {:?}", five);
                         known_patterns.insert(five.clone(), 5);
                         known_patterns_rev_lookup.insert(5, five.clone());
                     }
